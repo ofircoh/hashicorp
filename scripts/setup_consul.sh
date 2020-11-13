670794904d07309134a58791841c925e26a52cf8
Ofir Cohen <ofir098987@gmail.com>
2020-11-13 14:43:59 +0200

    setup script

--- scripts/setup_consul.sh
+++ scripts/setup_consul.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
 
+#cd into consul directory
 cd /opt/hashicorp
+
+# install docker-compose
 pip install docker-compose
+
+#build condul image and run the container
 docker-compose up -d --build
+
+#validate consul configuration file
 docker-compose exec -T consul bash -c "consul validate /consul/consul.d/server/config.json"
 
+#test kv input to consul
 export MASTER_TOKEN=9beb4f8e-7220-41d2-946c-b8a8e3574692
 curl --header "X-Consul-Token: $MASTER_TOKEN" --request PUT --data "ofir_test" http://127.0.0.1:8500/v1/kv/test
 curl --header "X-Consul-Token: $MASTER_TOKEN" --request GET http://127.0.0.1:8500/v1/kv/test
