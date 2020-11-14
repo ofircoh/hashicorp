360b26b513679a1a67cdfd95a2ecc2717fccc5e1
Ofir Cohen <ofir098987@gmail.com>
2020-11-14 11:44:04 +0200

    fixed init setup script

--- scripts/init_setup_consul.sh
+++ scripts/init_setup_consul.sh
@@ -1,16 +1,19 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
 
 #cd into consul directory
 cd /opt/hashicorp
 
 # install docker-compose
-pip install docker-compose
+pip3.6 install docker-compose
+
+# remove any previous data
+rm -rf /opt/hashicorp/consul/data/*
 
 #build condul image and run the container
 docker-compose up -d --build
 
 #validate consul configuration file
-docker-compose exec -T consul bash -c "consul validate /consul/consul.d/server/config.json"
+docker-compose exec -T consul bash -c "consul validate /consul/consul.d/server/"
 
 #test kv input to consul
 export MASTER_TOKEN=9beb4f8e-7220-41d2-946c-b8a8e3574692
